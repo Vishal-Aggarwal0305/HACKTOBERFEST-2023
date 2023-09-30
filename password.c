51df4efc22c8c99ec7a921c063204c17507e70b5
vishal03052001 <72963634+vishal03052001@users.noreply.github.com>
2023-09-30 20:36:33 +0530

    Create password.c

---
+++ password.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+     int i;
+     char ch1, password[10];
+     printf("Enter your password:");
+     for(i=0; i<10; i++)
+     {
+             ch1=getch();
+             password[i]=ch1;
+      
+             if(ch1!=13)      //13 is ASCII of Enter key
+                   printf("*");
+             if(ch1 == 13)
+                   break;
+      }
+      password[i]='\0';
+      printf("\n%s", password);
+      return 0;
+}
